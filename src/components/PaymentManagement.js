8adfe2f8bf0222cad1a4d646625d3805e69f6cbb
CtalxD <ctalaryal22@gmail.com>
2025-02-13 12:37:05 +0545

    Add PaymentManagement component for handling payments

---
+++ src/components/PaymentManagement.js
@@ -0,0 +1,14 @@
+// src/components/Dashboard.tsx
+
+import React from 'react';
+
+const PaymentManagement = () => {
+  return (
+    <div className="Payments">
+      <h2>Welcome to the Payments</h2>
+      {/* Add more dashboard content here */}
+    </div>
+  );
+};
+
+export default PaymentManagement;
