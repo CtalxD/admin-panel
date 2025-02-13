e7b612721d8a9939f71fb89f717859259d4c73b6
CtalxD <ctalaryal22@gmail.com>
2025-02-13 12:37:49 +0545

    Add BusManagement component for managing bus operations

---
+++ src/components/BusManagement.js
@@ -0,0 +1,14 @@
+// src/components/Dashboard.tsx
+
+import React from 'react';
+
+const BusManagement = () => {
+  return (
+    <div className="Bus Management">
+      <h2>Welcome to the Bus Management</h2>
+      {/* Add more dashboard content here */}
+    </div>
+  );
+};
+
+export default BusManagement;
