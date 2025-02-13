6a9caab8aa15acbaad33eed9d03d33ed98387839
CtalxD <ctalaryal22@gmail.com>
2025-02-13 12:36:35 +0545

    Add UserManagement component for user dashboard

---
+++ src/components/UserManagement.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+const UserManagement = () => {
+  return (
+    <div className="User Management">
+      <h2>Welcome to the User Management</h2>
+      {/* Add more dashboard content here */}
+    </div>
+  );
+};
+
+export default UserManagement;
